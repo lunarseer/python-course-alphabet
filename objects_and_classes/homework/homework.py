43c0807241737572eac50c03afefc2917e271356
Igor Chopenko <igor.chopenko@gmail.com>
2019-05-27 14:10:37 +0300

    progress

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -49,76 +49,245 @@
 
 import random
 import uuid
-from . constants import CARS_PRODUCER, CARS_TYPES, TOWNS
+from operator import attrgetter
+from constants import CARS_PRODUCER, CARS_TYPES, TOWNS
 
 
 class Car:
-    mileage = 0
-    number = None
-    price = 0
-    producer = ""
-    type = ""
 
-    def __init__(self):
-        pass
+    def __init__(self, mileage: float, price: float):
+        self.producer = random.choice(CARS_PRODUCER)
+        self.type = random.choice(CARS_TYPES)
+        self.mileage = mileage
+        self.price = price
+        self.change_number()
+
+    def __repr__(self):
+        return "Car - type: {}, producer: {}, number: {}, mileage: {}, price: {}".format(
+            self.type,
+            self.producer,
+            self.number,
+            self.mileage,
+            self.price)
 
     def __eq__(self, other):
-        pass
+        """
+        comparing objects by price attribute
+        :param other: Car class object
+        :return: bool
+        """
+        return self.price == other.price
 
     def __lt__(self, other):
-        pass
+        """
+        comparing objects by price attribute
+        :param other: Car class object
+        :return: bool
+        """
+        return self.price < other.price
 
     def __gt__(self, other):
-        pass
+        """
+        comparing objects by price attribute
+        :param other: Car class object
+        :return: bool
+        """
+        return self.price > other.price
 
     def change_number(self):
-        pass
+        """
+        Randomly change car number
+        :return:
+        """
+        self.number = str(uuid.uuid4())
 
 
 class Garage:
 
-    town = ""
-    cars = []
-    places = 0
-    owner = None    #uuid - owner
+    def __init__(self, places: int, owner=None):
+        self.cars = []
+        self.places = 0
+        self.town = random.choice(TOWNS)
+        self.places = places
+        if owner:
+            self.set_owner(owner.register_id)
+        else:
+            self.owner = None
 
-    def __init__(self):
-        pass
+    def __repr__(self):
+        return "Garage - owner: {}, town: {}, places: {}, cars count: {}".format(
+            self.owner,
+            self.town,
+            self.places,
+            self.cars_count()
+        )
 
-    def add(self):
-        pass
+    def _car_numbers(self):
+        return [x.number for x in self.cars]
 
-    def remove(self):
-        pass
+    def set_owner(self, owner_id: str):
+        """
+        uses to set garage owner
+        :param owner_id: str(uuid.uuid4)
+        :return:
+        """
+        self.owner = owner_id
+
+    def cars_count(self):
+        """
+        uses to calculate total cars count
+        :return: float
+        """
+        return len(self.cars)
+
+    def add_car(self, car):
+        """
+        Uses to add any Car to garage
+        :param car: Car Object
+        :return:
+        """
+        if car.number not in self._car_numbers():
+            self.cars.append(car)
+            self.places -= 1
+            print("{} added to {}".format(car, self))
+        else:
+            print("{} already in {}".format(car, self))
+
+
+    def remove_car(self, car):
+        """
+        Uses to remove car from garage, if car exists in garage
+        :param car: any Car instatnce
+        :return:
+        """
+        if car.number in self._car_numbers():
+            self.cars = [c for c in self.cars if not c.number == car.number]
+            self.places += 1
+            print("{} removed from {}".format(car, self))
+        else:
+            print("{} not in {}".format(car, self))
 
     def hit_hat(self):
-        pass
+        """
+        uses to calculate garage's cars total cost
+        :return: float
+        """
+        return sum([x.price for x in self.cars])
 
 
 class Cesar:
 
-    def __init__(self):
-        self.name = ""
+    def __init__(self, name: str):
+        self.name = name
         self.garages = []
-        self.register_id = None    #uuid
+        self.register_id = str(uuid.uuid4())
+
+    def __repr__(self):
+        return "{}, id: {}, garages: {}".format(self.name, self.register_id, self.garages_count())
 
     def __eq__(self, other):
-        pass
+        return self.hit_hat() == other.hit_hat()
 
     def __lt__(self, other):
-        pass
+        return self.hit_hat() < other.hit_hat()
 
     def __gt__(self, other):
-        pass
+        return self.hit_hat() > other.hit_hat()
+
+    def _have_free_place(self):
+        """
+        uses to get garage with max free spaces
+        :return: Garage
+        """
+        if len(self.garages):
+            return max([x for x in self.garages if x.places > 0], key=attrgetter("places"))
+        else:
+            print("{} do not have enough free places in garages!".format(self))
+            return None
+
+    def _all_cars(self):
+        """
+        uses to get all cars from all owner's garages
+        :return: Cars List
+        """
+        all_cars = []
+        for garage in self.garages:
+            all_cars += garage.cars
+        return all_cars
+
+    def add_garage(self, garage: Garage):
+        """
+        uses to add garage to owner
+        :param garage: Garage
+        :return:
+        """
+        if garage:
+            garage.set_owner(self.register_id)
+            self.garages.append(garage)
+
 
     def hit_hat(self):
-        pass
+        """
+        uses to calculate sum of owners's cars cost
+        :return: float
+        """
+        return sum([x.price for x in self._all_cars()])
 
     def garages_count(self):
-        pass
+        """
+        uses to calculate garages count
+        :return: float
+        """
+        return len(self.garages)
 
     def cars_count(self):
-        pass
+        """
+        uses for calculate cars count for all garages
+        :return: float
+        """
+        return len(self._all_cars())
 
-    def add_car(self):
-        pass
+    def add_car(self, car: Car):
+        """
+        uses to add car to owner's garage
+        :param car: Car
+        :return:
+        """
+        garage = self._have_free_place()
+        if garage:
+            garage.add_car(car)
+
+
+car1 = Car(mileage=45000, price=8000)
+car2 = Car(mileage=85000, price=16000)
+car3 = Car(mileage=15000, price=25000)
+car4 = Car(mileage=11000, price=20000)
+print(car1)
+car1.change_number()
+print(car1)
+print(car2)
+print(car1 < car2)
+
+cesar1 = Cesar(name="Rob the Selfish")
+g1 = Garage(places=16)
+g2 = Garage(places=12)
+g1.add_car(car1)
+g1.add_car(car2)
+
+cesar1.add_garage(garage=g1)
+cesar1.add_garage(garage=g2)
+
+g2.add_car(car3)
+g2.add_car(car=car4)
+
+g1.remove_car(car2)
+
+print(g1)
+print(g2)
+
+print(cesar1)
+print(cesar1.hit_hat())
+car5 = Car(mileage=11000, price=18000)
+cesar1.add_car(car5)
+print(cesar1._all_cars())
+print(cesar1.hit_hat())
